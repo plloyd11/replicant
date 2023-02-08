abba253f89b361dfcd0ca1f328dec0d362449d35
Pete Lloyd <plloyd11@gmail.com>
2023-02-08 18:09:46 -0500

    shows

--- src/components/GlobalNav.svelte
+++ src/components/GlobalNav.svelte
@@ -12,6 +12,10 @@
             anchor: '#videos',
         },
         {
+            name: 'shows',
+            anchor: '#shows',
+        },
+        {
             name: 'about',
             anchor: '#about',
         },
@@ -24,13 +28,13 @@
 </script>
 
 <header class="shadow z-10 w-full sm:fixed sm:top-5 sm:max-w-[calc(100%-80px)] sm:left-[40px]">
-    <nav aria-label="Top" class="py-2 bg-replicant-800 rounded relative">
+    <nav aria-label="Top" class="relative py-2 rounded bg-replicant-800">
         <div
-            class="px-4 sm:px-6 lg:px-8 w-full h-full z-10 relative grid grid-cols-2 sm:grid-cols-3"
+            class="relative z-10 grid w-full h-full grid-cols-2 px-4 sm:px-6 lg:px-8 sm:grid-cols-3"
         >
-            <div class="hidden sm:flex items-center space-x-8">
+            <div class="items-center hidden space-x-8 sm:flex">
                 {#each navItems as navItem}
-                    <a href={navItem.anchor} class="text-lg text-replicant-200 font-medium">
+                    <a href={navItem.anchor} class="text-lg font-medium text-replicant-200">
                         {navItem.name}
                     </a>
                 {/each}
@@ -42,7 +46,7 @@
                 </a>
             </div>
             <div class="flex items-center justify-end space-x-8">
-                <div class="hidden sm:flex items-center space-x-2 cursor-pointer">
+                <div class="items-center hidden space-x-2 cursor-pointer sm:flex">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         class="h-7 w-7 text-replicant-200"
@@ -57,14 +61,14 @@
                             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                         />
                     </svg>
-                    <button class="snipcart-customer-signin text-replicant-200 font-medium">
+                    <button class="font-medium snipcart-customer-signin text-replicant-200">
                         account
                     </button>
                 </div>
                 <div on:click={toggleMobileMenu} class="sm:hidden">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 text-replicant-100 cursor-pointer"
+                        class="w-6 h-6 cursor-pointer text-replicant-100"
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
@@ -92,7 +96,7 @@
                             d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                         />
                     </svg>
-                    <button class="text-replicant-200 font-medium">
+                    <button class="font-medium text-replicant-200">
                         <span class="hidden sm:inline-block">bag</span>
                         <span
                             class="ml-2 text-sm font-medium text-tertiaryColor group-hover:text-gray-800 snipcart-items-count"
@@ -111,18 +115,18 @@
                 class="absolute top-[88px] z-[15] inset-x-0 transition transform origin-top-right max-w-xs mx-auto"
             >
                 <div
-                    class="shadow-2xl rounded-br-lg rounded-bl-lg p-6 ring-1 ring-black ring-opacity-5 bg-replicant-700 divide-y-2 divide-gray-50"
+                    class="p-6 divide-y-2 rounded-bl-lg rounded-br-lg shadow-2xl ring-1 ring-black ring-opacity-5 bg-replicant-700 divide-gray-50"
                 >
                     <div class="flex flex-col space-y-4">
                         {#each navItems as navItem}
-                            <a href={navItem.anchor} class="text-lg text-replicant-200 font-medium">
+                            <a href={navItem.anchor} class="text-lg font-medium text-replicant-200">
                                 {navItem.name}
                             </a>
                         {/each}
                         <div
-                            class="flex justify-between items-center space-x-2 snipcart-customer-signin cursor-pointer"
+                            class="flex items-center justify-between space-x-2 cursor-pointer snipcart-customer-signin"
                         >
-                            <button class="text-replicant-200 font-medium"> account </button>
+                            <button class="font-medium text-replicant-200"> account </button>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 class="h-7 w-7 text-replicant-200"
